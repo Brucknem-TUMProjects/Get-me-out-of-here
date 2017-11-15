4ba60e2600d7b7ab59a1b7ff72abee3a4ef6148b
Marcel Bruckner <mbruckner94@gmail.com>
2017-11-15 18:34:12 +0100

    Basic Mouse Movement, Cube clickable

---
+++ Assets/_Scripts/CubeClick.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeClick : MonoBehaviour {
+
+    bool highlighted = false;
+    float clickStartTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnMouseDown()
+    {
+        clickStartTime = Time.time;
+    }
+
+    private void OnMouseUp()
+    {
+        if (Time.time - clickStartTime < 0.25f)
+        {
+            highlighted = !highlighted;
+            if (highlighted)
+                GetComponent<Renderer>().material.color = Color.red;
+            else
+                GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+}
---
+++ Assets/_Scripts/MoveCamera.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    [Header("Camera Settings")]
+    [Range(0, 50)]
+    public int turnSpeed;
+    [Range(0, 50)]
+    public int moveSpeed;
+    [Range(0, 50)]
+    public int zoomSpeed;
+
+    Vector3 oldMousePosition;
+
+    private bool isMoving;
+    private bool isRotating;
+    private float zoomFactor;
+    private bool mouseHeld;
+
+    private int moveButton = 0;
+    private int rotateButton = 1;
+    private int zoomButton = 2;
+
+    // Update is called once per frame
+    void Update () {
+        //print("Old: " + oldMousePosition + " - New: " + Input.mousePosition);
+
+        isMoving = Input.GetMouseButton(moveButton);
+        isRotating = Input.GetMouseButton(rotateButton);
+        zoomFactor = Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - oldMousePosition);
+
+        if (isMoving)
+        {
+            Vector3 move = new Vector3(-pos.x * moveSpeed, -pos.y * moveSpeed, 0);
+            transform.Translate(move, Space.Self);
+        }
+
+        if (isRotating)
+        {
+            transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
+            transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
+        }
+
+        if(zoomFactor != 0)
+        {
+            Vector3 move = transform.forward * zoomSpeed * zoomFactor;
+            transform.Translate(move, Space.World);
+        }
+
+        oldMousePosition = Input.mousePosition;
+    }
+}
