10b0c57301dddbbd3ed1256e2315df301631bc66
Marcel Bruckner <mbruckner94@gmail.com>
2018-01-31 16:00:00 +0100

    Removed Report

--- Assets/_Scripts/Algorithms/MyOwnImplementation.cs
+++ Assets/_Scripts/Algorithms/MyOwnImplementation.cs
@@ -35,57 +35,12 @@ public class MyOwnImplementation : Algorithm {
             value[(int)goal.x, (int)goal.y] = 0;
             policy[(int)goal.x, (int)goal.y] = '*';
             //openList = new List<Vector2>() { goal };
-            ValueFunction_Old(goal, new List<Vector2>() { goal });
+            ValueFunction(goal, new List<Vector2>() { goal });
         }
 
     }
-
-    private static void ValueFunction_New(Vector2 pos)
-    {
-        Iterations++;
-        openList.Remove(pos);
-
-        for (int i = 0; i < deltas.Count; i++)
-        {
-            Vector2 prev = pos - deltas[i];
-            if (prev.x >= 0 && prev.x < Width && prev.y >= 0 && prev.y < Height)
-            {
-                if (grid[(int)prev.x, (int)prev.y] == MaxCost)
-                {
-                    value[(int)prev.x, (int)prev.y] = MaxCost;
-                }
-                else if (goals.Contains(prev))
-                {
-                    value[(int)prev.x, (int)prev.y] = 0;
-                }
-                else if (value[(int)prev.x, (int)prev.y] > value[(int)pos.x, (int)pos.y] + grid[(int)pos.x, (int)pos.y])
-                {
-                    if (!openList.Contains(prev))
-                        PriorityAdd(prev);
-
-                    if (goals.Contains(prev))
-                    {
-                        value[(int)prev.x, (int)prev.y] = 0;
-                        policy[(int)prev.x, (int)prev.y] = '*';
-                    }
-                    else
-                    {
-                        value[(int)prev.x, (int)prev.y] = value[(int)pos.x, (int)pos.y] + grid[(int)pos.x, (int)pos.y];
-                        policy[(int)prev.x, (int)prev.y] = deltaNames[i];
-                    }
-                }
-            }
-        }
-
-        //Sort list
-        if (openList.Count > 0)
-        {
-            //Nimm den mit der niedrigsten value
-            ValueFunction_New(openList[0]/*, closed*/);
-        }
-    }
-
-    private static void ValueFunction_Old(Vector2 pos, List<Vector2> open/*, List<Vector2> closed*/)
+    
+    private static void ValueFunction(Vector2 pos, List<Vector2> open/*, List<Vector2> closed*/)
     {
         Iterations++;
         open.Remove(pos);
@@ -137,7 +92,7 @@ public class MyOwnImplementation : Algorithm {
                 }
             }
             //Nimm den mit der niedrigsten value
-            ValueFunction_Old (open[index], open/*, closed*/);
+            ValueFunction (open[index], open/*, closed*/);
         }
     }
 
